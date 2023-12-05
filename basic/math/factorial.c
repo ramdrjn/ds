71e74ce7fcc59d782779773f551de79e1748f27b
Ramesh Devarajan <2519490+ramdrjn@users.noreply.github.com>
2023-12-04 21:11:10 -0500

    initial log header

---
+++ basic/math/factorial.c
@@ -0,0 +1,15 @@
+#include "log.h"
+
+int factorial(int n)
+{
+    int fact = 0;
+    //n! = n * (n-1) * (n-2) .. * 2
+
+    return fact;
+}
+
+void main()
+{
+    int n = 4;
+    info("factorial value of %d is %d", n, factorial(n));
+}
---
+++ include/log.h
@@ -0,0 +1,30 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#include <stdio.h>
+
+enum LOG_LEVELS
+{
+    DEBUG,
+    INFO,
+    ERROR,
+};
+
+static enum LOG_LEVELS log_level = INFO;
+
+#define dbg(...) {                              \
+        if (log_level >= DEBUG)                 \
+            printf("\nDBG: "__VA_ARGS__);       \
+    }
+
+#define info(...) {                             \
+        if (log_level >= INFO)                  \
+            printf("\nINFO: "__VA_ARGS__);      \
+    }
+
+#define err(...) {                              \
+        if (log_level >= ERROR)                 \
+            printf("\nerr: "__VA_ARGS__);       \
+    }
+
+#endif /*_LOG_H_*/
