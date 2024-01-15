5aac7f40d9c73df3d357323b42460dc1412c17f6
Ramesh Devarajan <2519490+ramdrjn@users.noreply.github.com>
2024-01-15 15:38:17 -0500

    linear search updates

--- basic/algorithms/searching/linear.c
+++ basic/algorithms/searching/linear.c
@@ -1,61 +1,158 @@
 #include "utest/utest.h"
 #include "algo_analysis.h"
+#include <stdbool.h>
 
-int linear_search(int *n, int num_elements, int element)
+//also called sequemtial search. Tell if the value is present in the
+//list or not.
+bool linear_search_unsorted(int *n, int num_elements, int element)
 {
     //Iterate and find the element by comparing it with each element
 
-    int i;
     int loop_count = 0;
 
-    for (i=0; i< num_elements; i++)
+    for (int i=0; i< num_elements; i++)
     {
         loop_count++;
         algo_steps++;
 
         if(n[i] == element)
-            break;
+        {
+            dbg("loop count %d", loop_count);
+            return true;
+        }
     }
-
-    if(i == num_elements-1)
-        i=-1;
 
     dbg("loop count %d", loop_count);
 
-    return i;
+    return false;
 }
 
-UTEST(math, linearsearch_unsorted) {
+bool linear_search_sorted(int *n, int num_elements, int element)
+{
+    //Same as unsorted but check if the next value is greater than the
+    //element. If so then return false since the element is not present.
+
+    int loop_count = 0;
+
+    for (int i=0; i< num_elements; i++)
+    {
+        loop_count++;
+        algo_steps++;
+
+        if(n[i] == element)
+        {
+            dbg("loop count %d", loop_count);
+            return true;
+        }
+        if (n[i] > element)
+        {
+            dbg("loop count %d", loop_count);
+            return false;
+        }
+    }
+
+    dbg("loop count %d", loop_count);
+
+    return false;
+}
+
+UTEST(math, linearsearch_unsorted_unsorted_found) {
     int num_elements = 10;
     int n[10] = {9,8,7,6,5,9,4,3,2,1};
     int element = 4;
-    int result_index;
+    bool result;
 
     algo_steps = 0;
     algo_storage = 0;
 
-    result_index = linear_search(n, num_elements, element);
+    result = linear_search_unsorted(n, num_elements, element);
 
-    EXPECT_NE(result_index, -1);
-    EXPECT_EQ(element, n[result_index]);
+    EXPECT_EQ(result, 1);
 
     algo_time_analysis(num_elements, "n");
     //algo_space_analysis(num_elements, "1");
 }
 
-UTEST(math, linearsearch_sorted) {
+UTEST(math, linearsearch_unsorted_sorted_found) {
     int num_elements = 10;
     int n[10] = {1,2,3,4,5,6,7,8,9,9};
     int element = 4;
-    int result_index;
+    bool result;
 
     algo_steps = 0;
     algo_storage = 0;
 
-    result_index = linear_search(n, num_elements, element);
+    result = linear_search_unsorted(n, num_elements, element);
 
-    EXPECT_NE(result_index, -1);
-    EXPECT_EQ(element, n[result_index]);
+    EXPECT_EQ(result, 1);
+
+    algo_time_analysis(num_elements, "n");
+    //algo_space_analysis(num_elements, "1");
+}
+
+UTEST(math, linearsearch_unsorted_unsorted_notfound) {
+    int num_elements = 10;
+    int n[10] = {9,8,7,6,5,9,2,3,2,1};
+    int element = 4;
+    bool result;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    result = linear_search_unsorted(n, num_elements, element);
+
+    EXPECT_EQ(result, 0);
+
+    algo_time_analysis(num_elements, "n");
+    //algo_space_analysis(num_elements, "1");
+}
+
+UTEST(math, linearsearch_unsorted_sorted_notfound) {
+    int num_elements = 10;
+    int n[10] = {1,2,3,3,5,6,7,8,9,9};
+    int element = 4;
+    bool result;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    result = linear_search_unsorted(n, num_elements, element);
+
+    EXPECT_EQ(result, 0);
+
+    algo_time_analysis(num_elements, "n");
+    //algo_space_analysis(num_elements, "1");
+}
+
+UTEST(math, linearsearch_sorted_sorted_found) {
+    int num_elements = 10;
+    int n[10] = {1,2,3,4,5,6,7,8,9,9};
+    int element = 4;
+    bool result;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    result = linear_search_sorted(n, num_elements, element);
+
+    EXPECT_EQ(result, 1);
+
+    algo_time_analysis(num_elements, "n");
+    //algo_space_analysis(num_elements, "1");
+}
+
+UTEST(math, linearsearch_sorted_sorted_notfound) {
+    int num_elements = 10;
+    int n[10] = {1,2,3,3,5,6,7,8,9,9};
+    int element = 4;
+    bool result;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    result = linear_search_sorted(n, num_elements, element);
+
+    EXPECT_EQ(result, 0);
 
     algo_time_analysis(num_elements, "n");
     //algo_space_analysis(num_elements, "1");
