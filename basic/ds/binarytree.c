a82d8ebd270d8c3c317f7dcab4bdf7147637c10e
Ramesh Devarajan <2519490+ramdrjn@users.noreply.github.com>
2024-01-25 23:24:19 -0500

    binary tree delete

--- basic/ds/binarytree.c
+++ basic/ds/binarytree.c
@@ -2,8 +2,6 @@
 #include "algo_analysis.h"
 #include "pal.h"
 
-static int result_index=0;
-
 typedef struct tree_node tree_node_t;
 
 struct tree_node
@@ -49,7 +47,7 @@ void insert_binary_tree_root(int value, tree_node_t *root)
     root->value=value;
 }
 
-void insert_binary_tree(int value, tree_node_t *root)
+void insert_binary_tree_node(int value, tree_node_t *root)
 {
     tree_node_t *t = NULL;
     tree_node_t *curr_node = root;
@@ -99,45 +97,112 @@ void insert_binary_tree(int value, tree_node_t *root)
     }
 }
 
-void delete_binart_tree(int value, tree_node_t *root)
+/* Given a binary search tree and a value, this function
+   deletes the value and returns the new root */
+tree_node_t* delete_binary_tree_node(tree_node_t* root, int value)
 {
-    tree_node_t *t = NULL;
-    MEM_FREE(t);
-}
+    if (root == NULL)
+        return root;
 
-void binary_tree_pre_order_traversal(int *r, tree_node_t *root)
-{
-    if(root)
+    if (root->value > value)
     {
-        algo_steps++;
-        r[result_index++]=root->value;
-        //printf("%d", root->value);
-        binary_tree_pre_order_traversal(r, root->left);
-        binary_tree_pre_order_traversal(r, root->right);
+        root->left = delete_binary_tree_node(root->left, value);
+        return root;
+    }
+    else if (root->value < value)
+    {
+        root->right = delete_binary_tree_node(root->right, value);
+        return root;
+    }
+
+    // We reach here when root is the node
+    // to be deleted.
+
+    // If one of the children is empty
+    if (root->left == NULL)
+    {
+        tree_node_t* t = root->right;
+        MEM_FREE(root);
+        return t;
+    }
+    else if (root->right == NULL)
+    {
+        tree_node_t* t = root->left;
+        MEM_FREE(root);
+        return t;
+    }
+    else // If both children exist
+    {
+        tree_node_t* succParent = root;
+
+        // Find successor
+        tree_node_t* succ = root->right;
+        while (succ->left != NULL)
+        {
+            succParent = succ;
+            succ = succ->left;
+        }
+
+        // Delete successor.  Since successor
+        // is always left child of its parent
+        // we can safely make successor's right
+        // right child as left of its parent.
+        // If there is no succ, then assign
+        // succ->right to succParent->right
+        if (succParent != root)
+            succParent->left = succ->right;
+        else
+            succParent->right = succ->right;
+
+        // Copy Successor Data to root
+        root->value = succ->value;
+
+        // Delete Successor and return root
+        MEM_FREE(succ);
+        return root;
     }
 }
 
-void binary_tree_post_order_traversal(int *r, tree_node_t *root)
+/* Depth first search */
+void binary_tree_pre_order_traversal_recursive(int *r, int *index,
+                                               tree_node_t *root)
 {
     if(root)
     {
         algo_steps++;
-        binary_tree_post_order_traversal(r, root->left);
-        binary_tree_post_order_traversal(r, root->right);
-        r[result_index++]=root->value;
+        r[*index]=root->value;
+        *index=*index+1;
+        //printf("%d", root->value);
+        binary_tree_pre_order_traversal_recursive(r, index, root->left);
+        binary_tree_pre_order_traversal_recursive(r, index, root->right);
+    }
+}
+
+void binary_tree_post_order_traversal_recursive(int *r, int *index,
+                                                tree_node_t *root)
+{
+    if(root)
+    {
+        algo_steps++;
+        binary_tree_post_order_traversal_recursive(r, index, root->left);
+        binary_tree_post_order_traversal_recursive(r, index, root->right);
+        r[*index]=root->value;
+        *index=*index+1;
         //printf("%d", root->value);
     }
 }
 
-void binary_tree_in_order_traversal(int *r, tree_node_t *root)
+void binary_tree_in_order_traversal_recursive(int *r, int *index,
+                                              tree_node_t *root)
 {
     if(root)
     {
         algo_steps++;
-        binary_tree_in_order_traversal(r, root->left);
-        r[result_index++]=root->value;
+        binary_tree_in_order_traversal_recursive(r, index, root->left);
+        r[*index]=root->value;
+        *index=*index+1;
         //printf("%d", root->value);
-        binary_tree_in_order_traversal(r, root->right);
+        binary_tree_in_order_traversal_recursive(r, index, root->right);
     }
 }
 
@@ -170,6 +235,15 @@ UTEST_F_TEARDOWN(ds)
     //deinit_binary_tree(utest_fixture->root);
 }
 
+/*
+                   6
+              /        \
+             3         10
+            /  \       /
+           1    5     8
+            \  /    /   \
+             2 4   7     9
+ */
 UTEST_F(ds, binarytree_insert)
 {
     int num_elements = 10;
@@ -181,23 +255,23 @@ UTEST_F(ds, binarytree_insert)
     insert_binary_tree_root(n[0], utest_fixture->root);
 
     for(int i=1; i<num_elements; i++)
-        insert_binary_tree(n[i], utest_fixture->root);
+        insert_binary_tree_node(n[i], utest_fixture->root);
 
     algo_time_analysis(num_elements, "n");
     algo_space_analysis(num_elements, "n");
 }
 
-UTEST_F(ds, binarytree_pre_order_traversal)
+UTEST_F(ds, binarytree_pre_order_traversal_recursive)
 {
     int num_elements = 10;
     int r[10] = {0};
     int expected[10] = {6,3,1,2,5,4,10,8,7,9};
+    int index=0;
 
     algo_steps = 0;
     algo_storage = 0;
 
-    result_index=0;
-    binary_tree_pre_order_traversal(r, utest_fixture->root);
+    binary_tree_pre_order_traversal_recursive(r, &index, utest_fixture->root);
     for(int i=0; i<num_elements; i++)
         EXPECT_EQ(r[i], expected[i]);
 
@@ -205,17 +279,17 @@ UTEST_F(ds, binarytree_pre_order_traversal)
     algo_space_analysis(num_elements, "n");
 }
 
-UTEST_F(ds, binarytree_post_order_traversal)
+UTEST_F(ds, binarytree_post_order_traversal_recursive)
 {
     int num_elements = 10;
     int r[10] = {0};
     int expected[10] = {2,1,4,5,3,7,9,8,10,6};
+    int index=0;
 
     algo_steps = 0;
     algo_storage = 0;
 
-    result_index=0;
-    binary_tree_post_order_traversal(r, utest_fixture->root);
+    binary_tree_post_order_traversal_recursive(r, &index, utest_fixture->root);
     for(int i=0; i<num_elements; i++)
         EXPECT_EQ(r[i], expected[i]);
 
@@ -223,17 +297,132 @@ UTEST_F(ds, binarytree_post_order_traversal)
     algo_space_analysis(num_elements, "n");
 }
 
-UTEST_F(ds, binarytree_in_order_traversal)
+UTEST_F(ds, binarytree_in_order_traversal_recursive)
 {
     int num_elements = 10;
     int r[10] = {0};
     int expected[10] = {1,2,3,4,5,6,7,8,9,10};
+    int index=0;
 
     algo_steps = 0;
     algo_storage = 0;
 
-    result_index=0;
-    binary_tree_in_order_traversal(r, utest_fixture->root);
+    binary_tree_in_order_traversal_recursive(r, &index, utest_fixture->root);
+    for(int i=0; i<num_elements; i++)
+        EXPECT_EQ(r[i], expected[i]);
+
+    algo_time_analysis(num_elements, "n");
+    algo_space_analysis(num_elements, "n");
+}
+
+/*
+                   6
+              /        \
+             3         10
+            /  \       /
+           1    5     8
+            \  /    /   \
+             2 4   7     9 <-
+ */
+UTEST_F(ds, binarytree_delete_node_leaf)
+{
+    int num_elements=10;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    delete_binary_tree_node(utest_fixture->root, 9);
+
+    algo_time_analysis(num_elements, "n");
+    algo_space_analysis(num_elements, "n");
+}
+
+/*
+                   6
+              /        \
+             3         10
+            /  \       /
+           1    5 <-  8
+            \  /    /
+             2 4   7
+ */
+UTEST_F(ds, binarytree_delete_node_1_child)
+{
+    int num_elements=10;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    delete_binary_tree_node(utest_fixture->root, 5);
+
+    algo_time_analysis(num_elements, "n");
+    algo_space_analysis(num_elements, "n");
+}
+
+/*
+                   6
+              /        \
+             3         10
+            /  \       / \
+           1    4     8  11 <-
+            \        /
+             2      7
+ */
+UTEST_F(ds, binarytree_insert_after_delete)
+{
+    int num_elements=10;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    insert_binary_tree_node(11, utest_fixture->root);
+
+    algo_time_analysis(num_elements, "n");
+    algo_space_analysis(num_elements, "n");
+}
+
+/*
+                   6 <-
+              /        \
+             3         10
+            /  \       / \
+           1    4     8  11
+            \        /
+             2      7
+ */
+UTEST_F(ds, binarytree_delete_node_2_child)
+{
+    int num_elements=10;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    delete_binary_tree_node(utest_fixture->root, 6);
+
+    algo_time_analysis(num_elements, "n");
+    algo_space_analysis(num_elements, "n");
+}
+
+/*
+                   7
+              /        \
+             3         10
+            /  \       / \
+           1    4     8  11
+            \
+             2
+ */
+UTEST_F(ds, binarytree_in_order_traversal_recursive_after_delete_insert)
+{
+    int num_elements = 8;
+    int r[8] = {0};
+    int expected[8] = {1,2,3,4,7,8,10,11};
+    int index=0;
+
+    algo_steps = 0;
+    algo_storage = 0;
+
+    binary_tree_in_order_traversal_recursive(r, &index, utest_fixture->root);
     for(int i=0; i<num_elements; i++)
         EXPECT_EQ(r[i], expected[i]);
 
