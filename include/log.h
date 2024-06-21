c9744e6b649e869a7bf43cd9fc674bd7536a7480
RD <2519490+ramdrjn@users.noreply.github.com>
2024-06-20 21:39:45 -0400

    debug val log

--- include/log.h
+++ include/log.h
@@ -13,22 +13,22 @@ enum LOG_LEVELS
 static enum LOG_LEVELS log_level = INFO;
 
 #define dbgval(...) {                           \
-        if (log_level >= DEBUG)                 \
-          printf(__VA_ARGS__);                  \
+        if (log_level <= DEBUG)                 \
+            printf(__VA_ARGS__);                \
     }
 
 #define dbg(...) {                              \
-        if (log_level >= DEBUG)                 \
+        if (log_level <= DEBUG)                 \
             printf("\nDBG: "__VA_ARGS__);       \
     }
 
 #define info(...) {                             \
-        if (log_level >= INFO)                  \
+        if (log_level <= INFO)                  \
             printf("\nINFO: "__VA_ARGS__);      \
     }
 
 #define err(...) {                              \
-        if (log_level >= ERROR)                 \
+        if (log_level <= ERROR)                 \
             printf("\nerr: "__VA_ARGS__);       \
     }
 
